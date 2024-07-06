618765725866ebed41c344dc09db5e1243879420
Jose Costa <jcosta@grupopie.com>
2024-07-06 07:19:53 +0000

    update read

--- src/app/core/plugins/reader/components/barcode-scanning/barcode.service.ts
+++ src/app/core/plugins/reader/components/barcode-scanning/barcode.service.ts
@@ -35,7 +35,7 @@ export class BarcodeService {
     word: string = '';
     listenScan:boolean = true;
 
-    allowedCharsRegex = /^[a-zA-Z0-9.!@#$%&'*()_+\-=\[\]{};':",./\\|<>? ]+$/;
+    allowedCharsRegex = /^[a-zA-Z0-9\-\._~:\/\?#\[\]@!$&'()*+,;=%!^| ]+$/;
 
   
     constructor(
